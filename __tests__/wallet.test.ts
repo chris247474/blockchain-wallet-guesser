3e5017b83e4d989673b807ef874a32a8d21d77d4
ctverceles <ctverceles@gmail.com>
2019-07-14 18:10:31 +0800

    creates string w 64 hex chars

--- __tests__/wallet.test.ts
+++ __tests__/wallet.test.ts
@@ -1,23 +1,54 @@
 import * as walletCrack from '../index';
 import { doesNotReject } from 'assert';
+const number_to_base = require("number-to-base");
 
 describe('Testing wallet private key guesser', () => {
+    const iterations = 100;
+    const hexLength = 64;
+    const errStr = "currentDigits param must have length property value less than or equal to 64";
+
     beforeEach(() => {
     });
 
     beforeAll(() => {
     });
 
-    test('number 1 should convert to hex string then convert back to number 1', () => {
-        const num = 1;
+    test('randomIntFromInterval test', () => {
+        for(var c = 0;c < iterations;c++) {
+            const ranNum = walletCrack.randomIntFromInterval();
+            expect(ranNum).toBeGreaterThanOrEqual(0);
+        }
+    })
 
-        const hexString = walletCrack.convertNumToHexString(num);
-        console.log('hexString:', hexString);
-        expect(hexString.length).toBe(16);
+    test('constructRemainingDigits test', () => {
+        const ranNum = walletCrack.randomIntFromInterval();
+        expect(ranNum).toBeGreaterThanOrEqual(0);
 
-        const newNum = walletCrack.convertHexStringToNum(hexString);
-        console.log('newNum:', newNum);
-        expect(newNum).toBe(num);
+        const stringNum = ranNum.toString();
+        expect(stringNum).toHaveLength(1);
+
+        const hexString = walletCrack.constructRemainingDigits(stringNum);
+        expect(hexString.length).toBe(hexLength);
+    })
+
+    test('random number should convert to hex string then convert back to the same random number', () => {
+        const min = 0;
+        const max = Math.pow(10, 10);
+        let num: number;
+
+        for(var c = 0;c < iterations;c++) {
+            try {
+                num = walletCrack.randomIntFromInterval(min, max);
+
+                const hexFromNum = walletCrack.convertNumToHexString(num);
+                expect(hexFromNum.length).toBe(hexLength);
+
+                const numFromHex = walletCrack.convertHexStringToNum(hexFromNum);
+                expect(numFromHex).toBe(num);
+            } catch (err) {
+                console.log('Iteration ' + c + 'Skipped: random number ' + num + 'w length greater than 64 generated');
+            }
+        }
     });
 
     afterEach(() => {
--- index.ts
+++ index.ts
@@ -1,14 +1,39 @@
+const number_to_base = require("number-to-base");
 
+const hexLength = 64;
 
-export const convertNumToHexString = (number: Number) => {
-    if (number < 0)
-    {
-      number = 0xFFFFFFFF + number + 1;
-    }
-  
-    return number.toString(16).toUpperCase();
+export const randomIntFromInterval = (min: number = 0, max:number = 10) => // min and max included
+{
+    return Math.floor(Math.random() * Math.floor(max));
 }
 
-export const convertHexStringToNum = (hexString: string) => {
+export const constructRemainingDigits = (currentDigits: string) => {
+    const hexDigitToAppend = "0";
+    let finalHex = "";
+    const digitLength = 64;
+    let iterations: number = digitLength - currentDigits.length;
+
+    if(digitLength > currentDigits.length) {
+        for(var c = 0;c < iterations;c++) {
+            finalHex += hexDigitToAppend;
+        }
+        finalHex += currentDigits;
+    } else {
+        throw new Error("currentDigits param must have length property value less than or equal to 64");
+    }
+
+    return finalHex;
+}
+
+export const convertNumToHexString = (number: number, base: number = 16) => {
+    const hex = number_to_base(number, base);
+    return constructRemainingDigits(hex);
+}
+
+export const convertHexStringToNum = (hexString: string, base: number = 16) => {
     return parseInt(hexString, 16);
+}
+
+export const createWalletPrivateKey = () => {
+    
 }
